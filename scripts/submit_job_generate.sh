a474f77e7cfc03cfb4251f6dc16ee9ba226763d9
Youssef Nashed <youssef.shady@gmail.com>
2023-04-21 13:53:13 -0700

    reducing the number of args for scripts/generate

--- scripts/submit_job_generate.sh
+++ scripts/submit_job_generate.sh
@@ -6,11 +6,11 @@
 #SBATCH --gpus=a100:1
 #SBATCH --mem=256000
 
-if [ $# -ne 4 ]; then
-  echo "ERROR USAGE: $0 <config-path> <dataset-path> <dataset-name> <model pdb file>"
-  echo You need to specify a train config.ini, a path for saving the dataset, a dataset name, and a PDB file
+if [ $# -ne 2 ]; then
+  echo "ERROR USAGE: $0 <config-path> <out-starfile-path>"
+  echo You need to specify a train config.ini, a path for saving the dataset (starfile)
   exit 1
 fi
 
 singularity exec -B /sdf --nv /sdf/group/ml/CryoNet/singularity_images/animate_latest.sif \
-            python src/experiment_scripts/generate_dataset.py -c "$1" --sim_starfile_dir "$2" --experiment_name "$3" --atomic_pdb "$4"
+            python src/experiment_scripts/generate_dataset.py -c "$1" --sim_starfile_dir "$2"
